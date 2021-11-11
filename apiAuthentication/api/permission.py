34c23a0c8f554126b4debcc8cbc2819cbe171baa
Moin Khan <moinakmalkhan@gmail.com>
2021-11-11 18:39:34 +0500

    Learning about Authentication and Permission

--- apiAuthentication/api/permission.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class MyPermission(BasePermission):
-    def has_permission(self,request,view):
-        if request.method=="GET":
-            return True
-        return False
--- apiAuthentication/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Student
-
-
-class StudentSerializer(serializers.ModelSerializer):
-    # name = serializers.CharField(read_only=True)
-    class Meta:
-        model = Student
-        fields = "__all__"
